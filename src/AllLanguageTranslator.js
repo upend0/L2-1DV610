5e1845f3ca2e089f02de7bd16953446003cf0f0c
Maria Fredriksson <mf223wk@student.lnu.se>
2023-09-14 10:43:46 +0200

    Add basic translator + app that runs it

---
+++ src/AllLanguageTranslator.js
@@ -0,0 +1,52 @@
+/**
+ * Module for the class AllLanguageTranslator.
+ *
+ * @author Maria Fredriksson <mf223wk@student.lnu.se>
+ * @version 1.0.0
+ */
+
+/**
+ * Represents a translator that translates a string to All Language.
+ */
+export class AllLanguageTranslator {
+  /**
+   * Public method for translating a string to All Language.
+   *
+   * @param {string} stringToTranslate - The string to translate.
+   * @returns {string} The translated string.
+   */
+  translateString (stringToTranslate) {
+    // Split the input string into words
+    const words = stringToTranslate.split(' ')
+
+    // Translate each word and join them back into a sentence
+    const translatedWords = words.map(word => this.#translateWord(word))
+    const translatedString = translatedWords.join(' ').toLocaleLowerCase()
+
+    return translatedString
+  }
+
+  /**
+   * Method to translate a single word.
+   *
+   * @param {string} word - The word to translate.
+   * @returns {string} The translated word.
+   */
+  #translateWord (word) {
+    let index = 0
+    while (index < word.length && !this.#isVowel(word[index])) {
+      index++
+    }
+    return word.slice(index) + word.slice(0, index) + 'all'
+  }
+
+  /**
+   * Method to check if a character is a vowel.
+   *
+   * @param {char} char - The character to check.
+   * @returns {boolean} True if the character is a vowel, otherwise false.
+   */
+  #isVowel (char) {
+    return ['a', 'e', 'i', 'o', 'u', 'y', 'å', 'ä', 'ö'].includes(char.toLowerCase())
+  }
+}
---
+++ src/app.js
@@ -0,0 +1,19 @@
+import { AllLanguageTranslator } from './AllLanguageTranslator.js'
+
+// Read the input from the user
+// The first input is a number for a menu choice, where 1 is to translate to All Language and 2 is to translate from All Language.
+// The second input is the string to translate.
+const menuChoice = Number(process.argv[2])
+const stringToTranslate = process.argv.slice(3).join(' ')
+
+console.log(menuChoice)
+console.log(stringToTranslate)
+
+// Create a new allLanguageTranslator object
+const allLanguageTranslator = new AllLanguageTranslator()
+
+// Translate the string
+const translatedString = allLanguageTranslator.translateString(stringToTranslate)
+
+// Print the translated string
+console.log(translatedString)
