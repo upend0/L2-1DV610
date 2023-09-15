a0b549ef236a8d40b1b0e106963809ca43701db7
Maria Fredriksson <mf223wk@student.lnu.se>
2023-09-15 14:11:54 +0200

    Add three more output verification tests

--- test/AllLanguageTranslator.test.js
+++ test/AllLanguageTranslator.test.js
@@ -11,11 +11,34 @@ describe('AllLanguageTranslator', () => {
     const argument = 'hej på dig'
     const expectedOutput = 'ejhall åpall igdall'
     const result = allTranslator.translateToAllLanguage(argument)
+    // The test is written is this way because it checks for the output of the method - output verification
+    expect(result).toEqual(expectedOutput)
+  })
+
+  it('should translate a single character word "ö" to "öall"', () => {
+    const argument = 'ö'
+    const expectedOutput = 'öall'
+    const result = allTranslator.translateToAllLanguage(argument)
+    expect(result).toEqual(expectedOutput)
+  })
+
+  it('should translate a word that starts with a vowel correctly', () => {
+    const input = 'äpple'
+    const expectedOutput = 'äppleall'
+    const result = allTranslator.translateToAllLanguage(input)
+    expect(result).toEqual(expectedOutput)
+  })
+
+  it('should translate a word that ends with a vowel correctly', () => {
+    const input = 'sjö'
+    const expectedOutput = 'ösjall'
+    const result = allTranslator.translateToAllLanguage(input)
     expect(result).toEqual(expectedOutput)
   })
 
   it('should throw an error when the string is empty', () => {
     const argument = ''
+    // The test is written in this way because it checks for the expected behavior of the method - behavior verification
     expect(() => allTranslator.translateToAllLanguage(argument)).toThrow('The string is empty.')
   })
 
