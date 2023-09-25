8901712217f26cce61305ed7aa42ec90237f5afd
Maria Fredriksson <mf223wk@student.lnu.se>
2023-09-25 11:56:17 +0200

    Add tests for translating from robberLanguage to swedish

--- test/RobberLanguageTranslator.test.js
+++ test/RobberLanguageTranslator.test.js
@@ -76,4 +76,74 @@ describe('RobberLanguageTranslator', () => {
     const result = robberTranslator.translateToRobberLanguage(input)
     expect(result).toEqual(expectedOutput)
   })
+
+  it('should translate "kokafoffofe" to "kaffe"', () => {
+    const argument = 'kokafoffofe'
+    const expectedOutput = 'kaffe'
+    const result = robberTranslator.translateRobberToSwedish(argument)
+    expect(result).toEqual(expectedOutput)
+  })
+
+  it('should translate a single character word "ö" to "ö"', () => {
+    const argument = 'ö'
+    const expectedOutput = 'ö'
+    const result = robberTranslator.translateRobberToSwedish(argument)
+    expect(result).toEqual(expectedOutput)
+  })
+
+  it('should translate a word that starts with a vowel correctly', () => {
+    const input = 'äpoppoplole'
+    const expectedOutput = 'äpple'
+    const result = robberTranslator.translateRobberToSwedish(input)
+    expect(result).toEqual(expectedOutput)
+  })
+
+  it('should translate a word that ends with a vowel correctly', () => {
+    const input = 'sosjojö'
+    const expectedOutput = 'sjö'
+    const result = robberTranslator.translateRobberToSwedish(input)
+    expect(result).toEqual(expectedOutput)
+  })
+
+  it('should translate a word that contains a special character correctly', () => {
+    const input = 'idodé'
+    const expectedOutput = 'idé'
+    const result = robberTranslator.translateRobberToSwedish(input)
+    expect(result).toEqual(expectedOutput)
+  })
+
+  it('should translate a word with all lowercase letters correctly', () => {
+    const input = 'hohejoj'
+    const expectedOutput = 'hej'
+    const result = robberTranslator.translateRobberToSwedish(input)
+    expect(result).toEqual(expectedOutput)
+  })
+
+  it('should translate a word with all uppercase letters correctly', () => {
+    const input = 'HOHEJOJ'
+    const expectedOutput = 'hej'
+    const result = robberTranslator.translateRobberToSwedish(input)
+    expect(result).toEqual(expectedOutput)
+  })
+
+  it('should handle leading and trailing spaces correctly', () => {
+    const input = '  hohejoj   '
+    const expectedOutput = 'hej'
+    const result = robberTranslator.translateRobberToSwedish(input)
+    expect(result).toEqual(expectedOutput)
+  })
+
+  it('should handle multiple spaces between words correctly', () => {
+    const input = 'hohejoj    popå    dodigog'
+    const expectedOutput = 'hej på dig'
+    const result = robberTranslator.translateRobberToSwedish(input)
+    expect(result).toEqual(expectedOutput)
+  })
+
+  it('should handle mixed case input correctly', () => {
+    const input = 'Hohejoj Popå Dodigog'
+    const expectedOutput = 'hej på dig'
+    const result = robberTranslator.translateRobberToSwedish(input)
+    expect(result).toEqual(expectedOutput)
+  })
 })
