c1ee3f5661ca20ffe789326220aaeaffa50c675b
Maria Fredriksson <mf223wk@student.lnu.se>
2023-10-30 13:37:35 +0100

    Refactor app.js to export an object with all the translators in

--- src/app.js
+++ src/app.js
@@ -1,23 +1,15 @@
 import { AllLanguageTranslator } from './AllLanguageTranslator.js'
+import { FigLanguageTranslator } from './FigLanguageTranslator.js'
+import { ILanguageTranslator } from './ILanguageTranslator.js'
+import { PLanguageTranslator } from './PLanguageTranslator.js'
+import { RobberLanguageTranslator } from './RobberLanguageTranslator.js'
 
-try {
-  // Read the input from the user
-  // The first input is a number for a menu choice, where 1 is to translate to All Language and 2 is to translate from All Language.
-  // The second input is the string to translate.
-  const menuChoice = Number(process.argv[2])
-  const stringToTranslate = process.argv.slice(3).join(' ')
+// & Kanske byta namn till TheAllLanguageTranslator eller ThePretendAllLanguageTranslator...?
 
-  console.log(menuChoice)
-  console.log(stringToTranslate)
-
-  // Create a new allLanguageTranslator object
-  const allLanguageTranslator = new AllLanguageTranslator()
-
-  // Translate the string
-  const translatedString = allLanguageTranslator.translateToAllLanguage(stringToTranslate)
-
-  // Print the translated string
-  console.log(translatedString)
-} catch (error) {
-  console.error(error)
+export const translator = {
+  allLanguageTranslator: new AllLanguageTranslator(),
+  figLanguageTranslator: new FigLanguageTranslator(),
+  iLanguageTranslator: new ILanguageTranslator(),
+  pLanguageTranslator: new PLanguageTranslator(),
+  robberLanguageTranslator: new RobberLanguageTranslator()
 }
